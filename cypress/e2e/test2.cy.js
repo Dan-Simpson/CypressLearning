03385b62a499c81283081af53e95f5c90cb68f5c
Dan Simpson <daniel.simpson@alcumus.com>
2024-06-11 13:17:34 +0100

    Updating test2.cy.js

---
+++ cypress/e2e/test2.cy.js
@@ -0,0 +1,23 @@
+describe('Cypress learning', () => {
+
+    beforeEach('visit url', () => {
+        cy.visit('/')
+    })
+
+    it('navigate to safesupplier', () => {
+      
+    })
+  
+    it('click continue button', () => {
+
+        cy.get('[data-testid="continue-button"]').click();
+    })
+
+    it('navigate to safesupplier', () => {
+
+      })
+    
+    it('click continue button', () => {
+          cy.get('[data-testid="continue-button"]').click();
+    })
+})
