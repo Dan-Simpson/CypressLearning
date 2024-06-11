c9147d513b5e5df35b1bcb55d57b2c58846cea80
Dan Simpson <daniel.simpson@alcumus.com>
2024-06-11 14:52:52 +0100

    updating the url

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -1,5 +1,5 @@
 describe('Cypress learning', () => {
   it('Example test written title modification', () => {
-    cy.visit('https://example.cypress.io')
+    cy.visit('https://www.google.com')
   })
 })
