e5c759bf922e8df08241f48d4b4d61888b76a263
Dan Simpson <daniel.simpson@alcumus.com>
2024-07-23 13:57:10 +0100

    updated test

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -1,6 +1,22 @@
 describe('Cypress learning', () => {
-  it('Example test written title modification', () => {
-    cy.visit('https://example.cypress.io')
-    cy.get('selector').click();
+
+  it('test one', () => {
+    const firstName = 'Bob'
+    cy.visit('https://safesupplier-registration-main.alcumusdev.net/')
+    cy.get('[data-testid="continue-button"]').click();
+    cy.get('[id="firstName"]').type(firstName);
   })
+
+  it('test two', () => {
+    const firstName2 = 'Dan'
+    cy.visit('https://safesupplier-registration-main.alcumusdev.net/')
+    cy.get('[data-testid="continue-button"]').click();
+    cy.get('[id="firstName"]').type(firstName2);
+  })
+
+  // it('test three', () => {
+  //   cy.visit('https://safesupplier-registration-main.alcumusdev.net/')
+  //   cy.get('[data-testid="continue-button"]').click();
+  //   cy.get('[id="firstName"]').type(firstName);
+  // })
 })
